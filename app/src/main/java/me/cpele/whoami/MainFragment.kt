08f04e7f5b91cf8d74db177cd125bbc2f0450a54
cpele <geekarist@users.noreply.github.com>
2018-09-11 09:02:28 +0200

    Setup data binding, add 'sign in' button

--- app/src/main/java/me/cpele/whoami/MainFragment.kt
+++ app/src/main/java/me/cpele/whoami/MainFragment.kt
@@ -1,11 +1,13 @@
 package me.cpele.whoami
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import me.cpele.whoami.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,15 +17,21 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val binding = MainFragmentBinding.bind(view)
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
+    }
 }
--- app/src/main/java/me/cpele/whoami/MainViewModel.kt
+++ app/src/main/java/me/cpele/whoami/MainViewModel.kt
@@ -1,7 +1,11 @@
 package me.cpele.whoami
 
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun signIn() {
+        Log.d(javaClass.simpleName, "Yo")
+    }
 }
