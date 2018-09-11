d5afdaa8ce92e358ba9732c7d797db96c307c7cd
cpele <geekarist@users.noreply.github.com>
2018-09-11 08:52:33 +0200

    Initial import

---
+++ app/src/main/java/me/cpele/whoami/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.cpele.whoami
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/me/cpele/whoami/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.cpele.whoami
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
