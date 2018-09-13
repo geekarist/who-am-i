e6c5ecc8afe63e659b6c1e87d3ed9cf1a057becc
cpele <geekarist@users.noreply.github.com>
2018-09-13 08:59:38 +0200

    Get access token

--- app/src/main/java/me/cpele/whoami/MainFragment.kt
+++ app/src/main/java/me/cpele/whoami/MainFragment.kt
@@ -6,7 +6,9 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,6 +25,7 @@ class MainFragment : Fragment() {
         private const val AUTH_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth"
         private const val TOKEN_ENDPOINT = "https://www.googleapis.com/oauth2/v4/token"
         private const val REQUEST_CODE_AUTH_TOKEN = 42
+        private const val ACTION_AUTH_TOKEN_RESPONSE = "me.cpele.whoami.AUTH_TOKEN_RESPONSE"
     }
 
     private lateinit var viewModel: MainViewModel
@@ -46,9 +49,10 @@ class MainFragment : Fragment() {
         binding.setLifecycleOwner(this)
     }
 
+    private val authService by lazy { context?.applicationContext?.let { AuthorizationService(it) } }
+
     fun signIn() {
         context?.apply {
-            val authorizationService = AuthorizationService(applicationContext)
             val configuration = AuthorizationServiceConfiguration(Uri.parse(AUTH_ENDPOINT), Uri.parse(TOKEN_ENDPOINT))
             val request = AuthorizationRequest.Builder(
                     configuration,
@@ -56,21 +60,53 @@ class MainFragment : Fragment() {
                     ResponseTypeValues.CODE,
                     Uri.parse(REDIRECT_URI)
             ).setScope("profile").build()
-            val intent = Intent(applicationContext, MainActivity::class.java)
+            val intent = Intent(applicationContext, MainActivity::class.java).setAction(ACTION_AUTH_TOKEN_RESPONSE)
             val pendingIntent = PendingIntent.getActivity(applicationContext, REQUEST_CODE_AUTH_TOKEN, intent, 0)
-            authorizationService.performAuthorizationRequest(request, pendingIntent)
+            authService?.performAuthorizationRequest(request, pendingIntent)
         }
     }
 
     override fun onResume() {
         super.onResume()
         activity?.apply {
-            val response = AuthorizationResponse.fromIntent(intent)
-            val error = AuthorizationException.fromIntent(intent)
-            val authState = AuthState(response, error)
-            response?.apply {
-                Toast.makeText(applicationContext, "Received auth code: $authorizationCode", Toast.LENGTH_LONG).show()
+            if (intent.action == ACTION_AUTH_TOKEN_RESPONSE) {
+
+                val response = AuthorizationResponse.fromIntent(intent)
+                val error = AuthorizationException.fromIntent(intent)
+                val authState = AuthState(response, error)
+                Log.d(MainFragment::class.java.simpleName, "Auth code: ${response?.authorizationCode}")
+
+                authState.apply {
+                    persistTo(applicationContext)
+                    if (authorizationException != null) {
+                        Toast.makeText(
+                                applicationContext,
+                                "Auth error: ${authorizationException?.message}",
+                                Toast.LENGTH_LONG
+                        ).show()
+                        Log.w(MainFragment::class.java.simpleName, authorizationException)
+                    } else {
+                        authService?.let {
+                            performActionWithFreshTokens(it) { accessToken, _, _ ->
+                                Toast.makeText(context, "Access token: $accessToken", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+
+    private fun callUserInfo(accessToken: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+const val PREF_AUTH_STATE = "PREF_AUTH_STATE"
+
+private fun AuthState.persistTo(applicationContext: Context) {
+    PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putString(
+            PREF_AUTH_STATE,
+            jsonSerializeString()
+    ).apply()
 }
